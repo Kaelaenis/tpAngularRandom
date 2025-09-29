3116382ff53b4f08c5ff4388e84be1624328807a
Valentin Coulon <Valentin.coulon5@outlook.fr>
2025-09-29 09:53:06 +0200

    Add random function and simple view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RandomComponent } from './random/random.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RandomComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/random/random.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-random',
+  templateUrl: './random.component.html',
+  styleUrls: ['./random.component.css']
+})
+export class RandomComponent {
+  randomInt: number = 0;
+
+  intChosen: number = 0;
+  tryCount: number = 0;
+
+  maxtry: number = 5;
+  message: string = '';
+
+  generateRandomInt(): void {
+    this.randomInt = Math.floor(Math.random() * 10) + 1;
+  }
+
+  ngOnInit(): void {
+    this.generateRandomInt();
+  }
+
+  onButtonClick(): void {
+   while (this.intChosen !== this.randomInt && this.tryCount < this.maxtry) {
+      this.tryCount++;
+      if (this.intChosen < this.randomInt) {
+        this.message = 'Trop petit !';
+      } else if (this.intChosen > this.randomInt) {
+        this.message = 'Trop grand !';
+      }
+      return;
+    }
+    if (this.tryCount >= this.maxtry) {
+      this.message = `Perdu ! Le nombre était ${this.randomInt}.`;
+      return;
+    }
+    this.message = 'Gagné !';
+  }
+}
